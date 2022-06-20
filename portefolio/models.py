aaa51f202802f62f10aec6e43c916d40bafbf38d
Gabriel Fernandes <force2606@gmail.com>
2022-06-20 22:58:56 +0100

    Upload Image

--- portefolio/models.py
+++ portefolio/models.py
@@ -1,12 +1,20 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
 
+class Image(models.Model):
+    image_file = models.ImageField(upload_to='images')
+
 class Project(models.Model):
     name = models.CharField(max_length=200)
-    #logo_file_id = models.Column(models.Integer, models.ForeignKey('project.id'))
+    logo_image = models.OneToOneField(Image, on_delete=models.SET_NULL, null=True)
     description = models.CharField(max_length=500, null=True)
     keywords = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return self.name
+
+class File(models.Model):
+    filename = models.CharField(max_length=200, unique=True, null=False)
+
--- portefolio/views.py
+++ portefolio/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Project
+
 # Create your views here.
 def index(request):
     template = loader.get_template('portefolio/index.html')
+    
+    projects = Project.objects.select_related('logo_image').all()
+
     context = {
-        
+        'projects': projects
     }
+
     return HttpResponse(template.render(context, request))
