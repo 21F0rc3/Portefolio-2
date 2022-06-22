0c6061319701b3368bcd2981286249747bdbbb40
Gabriel Fernandes <force2606@gmail.com>
2022-06-22 23:42:28 +0100

    dattabase keys

--- app/settings.py
+++ app/settings.py
@@ -77,22 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
-    secrets = json.load(secrets_file)
-
-def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting))
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portefolio',
         'USER': 'postgres',
-        'PASSWORD': get_secret('DB_PASSWORD'),
+        'PASSWORD': os.environ["DB_PASSWORD"],
         'HOST': 'localhost',
     }
 }
